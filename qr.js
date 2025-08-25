9521b3cc9fc0b5f1b3d50a90f0fc420812fff0e3
tharusha-md777 <panelpaka@gmail.com>
2025-08-25 18:44:43 +0530

    Update qr.js

--- qr.js
+++ qr.js
@@ -78,7 +78,7 @@ text: desc,
 contextInfo: {
 externalAdReply: {
 title: "ᴛʜᴀʀᴜꜱʜᴀ-ᴍᴅ",
-thumbnailUrl: "https://i.ibb.co/5gQ13m8p/0ef38c1555c87f01.jpg",
+thumbnailUrl: "https://i.ibb.co/FpKrDJ1/Tharusha-Md.jpg",
 sourceUrl: "https://whatsapp.com/channel/0029Vb9LTRHInlqISdCfln45",
 mediaType: 1,
 renderLargerThumbnail: true
@@ -94,7 +94,7 @@ text: desc,
 contextInfo: {
 externalAdReply: {
 title: "ᴛʜᴀʀᴜꜱʜᴀ-ᴍᴅ",
-thumbnailUrl: "https://i.ibb.co/5gQ13m8p/0ef38c1555c87f01.jpg",
+thumbnailUrl: "https://i.ibb.co/FpKrDJ1/Tharusha-Md.jpg",
 sourceUrl: "https://whatsapp.com/channel/0029Vb9LTRHInlqISdCfln45",
 mediaType: 2,
 renderLargerThumbnail: true,
